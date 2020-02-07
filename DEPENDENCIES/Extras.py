66076447ed03f0c7b41cbf435df0c7192faff202
Sebastian Franco Ulloa <cebasfu93@gmail.com>
2020-02-07 16:41:02 +0100

    Removed deprecated classes for parameter administration

--- DEPENDENCIES/Extras.py
+++ DEPENDENCIES/Extras.py
@@ -212,7 +212,7 @@ class Parameters:
         if np.any(np.invert(bond_checks)):
             no_params_ndx = np.where(np.invert(bond_checks))[0]
             missing_pairs = ["{}-{}".format(lig_btypes[ndx], lig_btypes[ndx+1]) for ndx in no_params_ndx]
-            warn_txt = "ATTENTION. Missing parameters for bonds: {}".format(missing_pairs)
+            warn_txt = "ATTENTION. Missing parameters for bonds: {}".format(np.unique(missing_pairs))
             logger.warning(warn_txt)
 
     def check_angle_parameters(self, inp, lig1or2):
@@ -221,7 +221,7 @@ class Parameters:
         if np.any(np.invert(angle_checks)):
             no_params_ndx = np.where(np.invert(angle_checks))[0]
             missing_pairs = ["{}-{}-{}".format(lig_btypes[ndx], lig_btypes[ndx+1], lig_btypes[ndx+2]) for ndx in no_params_ndx]
-            warn_txt = "ATTENTION. Missing parameters for angles: {}".format(missing_pairs)
+            warn_txt = "ATTENTION. Missing parameters for angles: {}".format(np.unique(missing_pairs))
             logger.warning(warn_txt)
 
     def check_dihedral_parameters(self, inp, lig1or2):
@@ -230,7 +230,7 @@ class Parameters:
         if np.any(np.invert(dihedral_checks)):
             no_params_ndx = np.where(np.invert(dihedral_checks))[0]
             missing_pairs = ["{}-{}-{}-{}".format(lig_btypes[ndx], lig_btypes[ndx+1], lig_btypes[ndx+2], lig_btypes[ndx+3]) for ndx in no_params_ndx]
-            warn_txt = "ATTENTION. Missing parameters for dihedral: {}".format(missing_pairs)
+            warn_txt = "ATTENTION. Missing parameters for dihedral: {}".format(np.unique(missing_pairs))
             logger.warning(warn_txt)
 
     def check_missing_parameters(self, inp):
