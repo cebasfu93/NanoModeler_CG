f5d5bb81b32789e464a8fb7735425d4216090c9e
Sebastian Franco Ulloa <cebasfu93@gmail.com>
2020-02-20 14:09:56 +0100

    Implements normals for rectangular prism shape

--- DEPENDENCIES/shape_normals.py
+++ DEPENDENCIES/shape_normals.py
@@ -19,7 +19,14 @@ def cylinder_normal(xyz, inp):
     return normal
 
 def rectangular_prism_normal(xyz, inp):
-    return xyz
+    a, b, c = inp.core_rect_prism
+    plane_normals = np.identity(3)
+    pts_inplane = np.array([[a,0,0],[0,b,0],[0,0,c]])/2
+    D = -1*np.array([np.dot(plane_normal, pt_inplane) for plane_normal, pt_inplane in zip(plane_normals, pts_inplane)])
+    dists = np.divide(np.abs(np.dot(plane_normals,np.abs(xyz))+D), np.linalg.norm(plane_normals, axis=1))
+    normals = np.zeros(3)
+    normals[np.argmin(dists)] = np.sign(xyz[np.argmin(dists)])
+    return normals
 
 def rod_normal(xyz, inp):
     if (xyz[0]**2 + xyz[1]**2 + (np.abs(xyz[2])-inp.core_rod_params[1]/2)**2) < (inp.core_rod_params[0]**2):
