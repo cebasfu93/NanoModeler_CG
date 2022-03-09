b4f776f79824ee8e4f881d6a2ee1a8e979b35a4b
Sebastian Franco Ulloa <cebasfu93@gmail.com>
2022-03-09 14:45:31 +0100

    fixed crash when total charge is 0

--- DEPENDENCIES/writers.py
+++ DEPENDENCIES/writers.py
@@ -89,8 +89,9 @@ def top_writer(TMP, np_xyz, core_bonds, lig_bonds, lig_angles, lig_dihedrals, in
             f.write("{:>6d} {:>4} {:>5} {:>5} {:>5} {:>5} {:>12.6f}    {:>9.5f} ; qtot {:>6.3f}\n".format(at, btype, res, "LIG2", "B{}".format(j), at, charge, mass, q_tot))
 
     logger.info("Total charge of the system: {:.3f} e...".format(q_tot))
-    if q_tot%int(q_tot) != 0.0:
-        logger.warning("Beware, the total charge of the system is non-integer!")
+    if q_tot != 0:
+        if q_tot%int(q_tot) != 0.0:
+            logger.warning("Beware, the total charge of the system is non-integer!")
     logger.info("\tWriting [ bonds ]...")
     f.write("\n[ bonds ]\n")
     f.write(";  ai    aj funct           c0           c1\n")
